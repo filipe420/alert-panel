939b85ff085bf58b6a94156c61a02c2e43b03169
filipe420 <145150193+filipe420@users.noreply.github.com>
2024-10-09 08:33:52 -0300

    up

---
+++ src/components/PillButton/PillButton.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function PillButton({ text = "" , onClick = () => {} }) {
+    return (
+      <button className="bg-white rounded-full text-emerald-600 px-12 py-2"
+       onClick={onClick}
+      >
+        {text}
+      </button>
+    )
+}
